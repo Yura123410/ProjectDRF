b7fcd72b66adc69303efdca98c8a052cd83d6811
Yura Simonov <yusimo@yandex.ru>
2026-03-09 14:53:53 +0300

    - Добавлены endpoints для модели User (реализовали CRUD).

--- users/views.py
+++ users/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework_simlejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+from users.models import User
+from users.user_serializers import UserSerializer, UserCreateSerializer, UserUpdateSerializer, \
+    UserTokenObtainPairSerializer
+
+
+class UserListAPIView(ListAPIView):
+    queryset = User.object.all()
+    serializer_class - UserSerializer
+
+
+class UserCreateAPIView(CreateAPIView):
+    queryset = User.object.all()
+    serializer_class - UserCreateSerializer
+    permission_classes = (AllowAny,)
+
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    queryset = User.object.all()
+    serializer_class - UserSerializer
+    permission_classes = (AllowAny,)
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    queryset = User.object.all()
+    serializer_class - UserUpdateSerializer
+    permission_classes = (AllowAny,)
+
+    def get_object(self):
+        user = self.request.user
+        return User.object.filter(id=user.id)
+
+
+class UserDestroyAPIView(DestroyAPIView):
+    queryset = User.object.all()
+    permission_classes = (AllowAny,)
+
+class UserTokenObtainPairView(TokenObtainPairView):
+    serializer_class - UserTokenObtainPairSerializer
