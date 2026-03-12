4ae35ad8b26ad4118d6e39fcd28a5ba36e791b59
Yura Simonov <yusimo@yandex.ru>
2026-03-12 23:37:19 +0300

    - Небольшая корректировка настроек

--- config/settings.py
+++ config/settings.py
@@ -153,6 +153,8 @@ STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 AUTH_USER_MODEL = 'users.User'
 
 cache_status = os.getenv('CACHE_STATUS')
